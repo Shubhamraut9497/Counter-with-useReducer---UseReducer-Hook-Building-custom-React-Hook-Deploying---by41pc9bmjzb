f903bab8fa9844ec65df1ac8cbe5f2495b19fbc2
Shubhamraut9497 <114891419+Shubhamraut9497@users.noreply.github.com>
2023-02-04 21:14:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,24 @@ const App = () => {
 const [state,dispatch] = useReducer(counterReducer,{/*initial state to be placed here*/})
   return (
     <div id="main">
-
+         <span id="counter">{state}</span>
+      <br />
+      <button
+        id="increment-btn"
+        onClick={() => {
+          dispatch({ type: "Increment" });
+        }}
+      >
+        Increment
+      </button>
+      <button
+        id="decrement-btn"
+        onClick={() => {
+          dispatch({ type: "Decrement" });
+        }}
+      >
+        Decrement
+      </button>
     </div>
   )
 }
